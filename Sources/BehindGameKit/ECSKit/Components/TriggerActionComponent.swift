4027c5ca5e5fef28fe7e7e137e20f227583310af
Victor Vasconcelos <victorabroum@gmail.com>
2020-03-06 16:38:50 -0400

    Improve trigger component to work with pause system

--- Sources/BehindGameKit/ECSKit/Components/TriggerActionComponent.swift
+++ Sources/BehindGameKit/ECSKit/Components/TriggerActionComponent.swift
@@ -11,6 +11,12 @@ import SpriteKit
 
 public class TriggerActionComponent: GKComponent {
     
+    private var lastTimeUpdated: TimeInterval = 0
+    
+    private var action: (() -> Void)?
+    private var waitTime: TimeInterval = 2
+    private var isRecursive: Bool = false
+    
     override public init() {
         super.init()
     }
@@ -22,17 +28,25 @@ public class TriggerActionComponent: GKComponent {
     // Component System
     override public func update(deltaTime seconds: TimeInterval) {
         super.update(deltaTime: seconds)
+        if (CACurrentMediaTime() - self.lastTimeUpdated > 2) {
+            self.lastTimeUpdated = CACurrentMediaTime()
+            if let action = action {
+                action()
+            }
+        }
     }
     
     public func triggerAction(waitTime timeInterval: TimeInterval = 2, isRecursive recursive: Bool = false, action: (() -> Void)? = nil) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + timeInterval) {
-            action?()
-            if recursive {
-                self.triggerAction(waitTime: timeInterval, isRecursive: recursive, action: action)
-            }
-        }
+        action?()
+        self.action = action
+        self.waitTime = timeInterval
+        self.isRecursive = recursive
+//        if recursive {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + timeInterval) {
+//                self.triggerAction(waitTime: timeInterval, isRecursive: recursive, action: action)
+//            }
+//        }
         
     }
-    
 }
 
