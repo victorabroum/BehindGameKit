38be24c598f871b22515503beb940ce159be8600
Victor Vasconcelos <vfvasconcelos3010@gmail.com>
2020-03-15 21:18:31 -0400

    Create SKButtonNode custom node for SpriteKit++ module

---
+++ Sources/BehindGameKit/SpriteKitBuffed/CustomNodes/SKButtonNode.swift
@@ -0,0 +1,45 @@
+//
+//  SKButtonNode.swift
+//  
+//
+//  Created by Victor Vasconcelos on 15/03/20.
+//
+
+import Foundation
+import SpriteKit
+
+public class SKButtonNode: SKNode {
+    
+    public var buttonBackground: SKSpriteNode?
+    public var buttonLabel: SKLabelNode?
+    public var delegate: (()->Void)?
+    
+    public override init() {
+        super.init()
+        self.setupButtonNode()
+    }
+    
+    public init(background: SKSpriteNode, label: SKLabelNode, delegate: @escaping () -> Void) {
+        self.buttonLabel = label
+        self.buttonBackground = background
+        self.delegate = delegate
+        super.init()
+        self.addChild(buttonBackground!)
+        self.addChild(buttonLabel!)
+        self.setupButtonNode()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setupButtonNode()
+    }
+    
+    private func setupButtonNode() {
+        self.isUserInteractionEnabled = true
+    }
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.delegate?()
+    }
+    
+}
