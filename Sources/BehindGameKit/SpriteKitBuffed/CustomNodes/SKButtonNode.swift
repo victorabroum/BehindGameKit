6c027bb108643636135c247159eb55b16ec8c00c
Victor Vasconcelos <victorabroum@gmail.com>
2020-05-22 10:31:35 -0400

    Improve Button to perfom clicked animation

--- Sources/BehindGameKit/SpriteKitBuffed/CustomNodes/SKButtonNode.swift
+++ Sources/BehindGameKit/SpriteKitBuffed/CustomNodes/SKButtonNode.swift
@@ -14,15 +14,41 @@ public class SKButtonNode: SKNode {
     public var buttonLabel: SKLabelNode?
     public var delegate: (()->Void)?
     
+    public var clickedTexture: SKTexture?
+    public var unClickedTexture: SKTexture?
+    
+    private var isClicked: Bool = false {
+        didSet {
+            if isClicked {
+                if let texture = clickedTexture {
+                    buttonBackground?.run(.sequence([
+                        .animate(with: [texture], timePerFrame: 1)
+                    ]))
+                }
+            } else {
+                if let texture = unClickedTexture {
+                    buttonBackground?.run(.sequence([
+                        .animate(with: [texture], timePerFrame: 1)
+                    ]))
+                }
+            }
+        }
+    }
+    
     public override init() {
         super.init()
         self.setupButtonNode()
     }
     
-    public init(background: SKSpriteNode, label: SKLabelNode, delegate: @escaping () -> Void) {
+    public init(background: SKSpriteNode,
+                label: SKLabelNode,
+                clickedTexture: SKTexture? = nil,
+                delegate: @escaping () -> Void) {
         self.buttonLabel = label
         self.buttonBackground = background
         self.delegate = delegate
+        self.unClickedTexture = background.texture
+        self.clickedTexture = clickedTexture
         super.init()
         self.addChild(buttonBackground!)
         self.addChild(buttonLabel!)
@@ -43,10 +69,23 @@ public class SKButtonNode: SKNode {
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         UINotificationFeedbackGenerator().notificationOccurred(.success)
         self.delegate?()
+        isClicked = true
+    }
+    
+    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isClicked = false
+    }
+    
+    public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isClicked = false
     }
     #elseif os(OSX)
     public override func mouseDown(with event: NSEvent) {
         self.delegate?()
+        isClicked = true
+    }
+    public override func mouseUp(with event: NSEvent) {
+        isClicked = false
     }
     #endif
     
