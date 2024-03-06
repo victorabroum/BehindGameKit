2db183decfba6d840f606b4ac850a0f88f906ca9
Victor Vasconcelos <vfvasconcelos3010@gmail.com>
2024-03-06 13:15:58 -0400

    Fix the accessor to SKGameScene

--- Sources/BehindGameKit/SpriteKitBuffed/Scene/GameScene.swift
+++ Sources/BehindGameKit/SpriteKitBuffed/Scene/GameScene.swift
@@ -8,17 +8,17 @@
 import Foundation
 import SpriteKit
 
-public class SKGameScene: SKScene {
+open class SKGameScene: SKScene {
     
     public var entityManager: SKEntityManager?
-    private var lastUpdateTime : TimeInterval = 0
+    public var lastUpdateTime : TimeInterval = 0
     
-    public override func sceneDidLoad() {
+    open override func sceneDidLoad() {
         self.lastUpdateTime = 0
         self.entityManager = SKEntityManager(self)
     }
     
-    override public func update(_ currentTime: TimeInterval) {
+    open override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         
         // Initialize _lastUpdateTime if it has not already been
